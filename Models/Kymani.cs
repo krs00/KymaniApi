5cfa7f528d81b394010254fac52bc71d55b42e72
Kymani Stephens <kymanist00@gmail.com>
2023-08-06 17:56:03 -0700

    add kymani model

---
+++ Models/Kymani.cs
@@ -0,0 +1,9 @@
+namespace KymaniApi.Models
+{
+  public class Kymani
+  {
+    public int KymaniId { get; set; }
+    public string Mood { get; set; }
+    public int PowerLevel { get; set; }
+  }
+}
