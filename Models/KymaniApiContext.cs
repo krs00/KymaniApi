003351518c9277d10416c113c42bb8d8fd36f7fb
Kymani Stephens <kymanist00@gmail.com>
2023-08-06 22:47:36 -0700

    add context file

--- Models/KymaniApiContext.cs
+++ Models/KymaniApiContext.cs
@@ -5,6 +5,19 @@ namespace KymaniApi.Models
   public class KymaniApiContext : DbContext
   {
     public DbSet<Kymani> Kymanis { get; set; }
-    public KymaniApiContext(DbContextOptions<KymaniApiContext> options) : base(options) {}
+    public KymaniApiContext(DbContextOptions<KymaniApiContext> options) : base(options) 
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Kymani>()
+        .HasData(
+          new Kymani { KymaniId = 1, Mood = "Happy", PowerLevel = 12 },
+          new Kymani { KymaniId = 2, Mood = "Cool", PowerLevel = 9001 } 
+        );
+    }
+
+
   }
 }
