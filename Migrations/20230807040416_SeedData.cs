acd7a6956cc3668dd1166408638664e1eb3252d7
Kymani Stephens <kymanist00@gmail.com>
2023-08-06 22:46:17 -0700

    add migrations

---
+++ Migrations/20230807040416_SeedData.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KymaniApi.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Kymanis",
+                columns: new[] { "KymaniId", "Mood", "PowerLevel" },
+                values: new object[] { 1, "Happy", 12 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Kymanis",
+                keyColumn: "KymaniId",
+                keyValue: 1);
+        }
+    }
+}
